de5c84e26a28e5f4b7a7ca5c8a4a8294b004f95a
Milenia Putri <mileniaptr@gmail.com>
2021-01-18 10:52:02 +0700

    Initial commit

---
+++ app/src/main/java/com/milenia/project_ida/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.milenia.project_ida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
